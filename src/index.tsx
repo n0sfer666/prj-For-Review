3ebf6aba02e673de18d54878982a8ddcb93df77a
Petr Khasanov <n0sfer666@yandex.ru>
2022-03-28 15:44:38 +0700

    fixed index.html + added fetch users data

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  // const getUsers =
+  const users: Array<Record<string, unknown>> = [];
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json(), (error) => console.error(error))
+    .then((data: Array<Record<string, unknown>>) => data.forEach(
+      (user: Record<string, unknown>) => {
+        users.push(user);
+      },
+    ));
+
   return (
-    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div>
+      test
+    </div>
   );
 }
 
